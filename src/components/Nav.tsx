6857e8ad7681d1b267f221ed3773e4c6865f3f24
Barth <workwithbarth@gmail.com>
2025-12-13 22:16:38 +0100

    Layout Improvements

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,9 +4,9 @@ export default function Nav() {
   return (
     <nav className="text-[var(--content-tertiary)] px-8 py-4 flex gap-6">
       <div className= "w-[32px] h-[32px] bg-[var(--background-secondary)]"></div>
-      <Link to="/" className="hover:text-[var(--content-primary)]">Home</Link>
-      <Link to="/projects" className="hover:text-[var(--content-primary)]">Projects</Link>
-      <Link to="/reading-list" className="hover:text-[var(--content-primary)]">Reading</Link>
+      <Link to="/" className="label-m hover:text-[var(--content-primary)]">Home</Link>
+      <Link to="/projects" className="label-m hover:text-[var(--content-primary)]">Projects</Link>
+      <Link to="/reading-list" className="label-m hover:text-[var(--content-primary)]">Reading</Link>
     </nav>
   );
 }
--- src/pages/ReadingList.tsx
+++ src/pages/ReadingList.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
       <div className="flex flex-col lg:flex-row w-full gap-6 lg:gap-8 h-auto lg:justify-left lg:row justify-center">
         <InfoBlock
-          title="Projects"
+          title="Reading List"
           number={12}
           description="Completed interactive projects and experiments."></InfoBlock>
           <div className= "w-full aspect-square bg-[var(--background-secondary)] transform [transform:perspective(500px)_skewX(-24deg)]"></div>
