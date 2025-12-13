2664b2bfe0850b178baac01df994540634e56bae
Barth <workwithbarth@gmail.com>
2025-12-14 00:24:06 +0100

    style(ui): refine interface motion and layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,35 @@
 export default function Footer() {
     return (
-      <footer className="p-6 mt-auto">
-        <div className="max-w-[1440px] mx-auto flex flex-col items-start gap-4">
-          <p className="label-s text-[var(--content-primary)]">&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
-  
-          <div className='items-start text-left'>
-            <div className="flex flex-col gap-2 label-s text-[var(--content-primary)]">
-              <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">
-              X (Twitter)
-              </a>
-              <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">
-              GitHub
-              </a>
-              <a href="mailto:you@example.com" className="hover:underline">
-              Email
-              </a>
-            </div>
+      <footer className="w-full p-4 md:p-6">
+        <div className="max-w-[1440px] flex flex-col items-start gap-4 rounded-[var(--radius-lg)] bg-[var(--background-secondary)]">
+          <div className='flex flex-col items-start text-left gap-12 p-6 md:p-10'>
+            <div className="flex flex-col gap-3">
+              <div className="label-s text-[var(--content-tertiary)]">Links</div>
+              <div className="flex flex-col gap-2 label-m text-[var(--content-primary)]">
+                <div className="flex flex-row gap-2">
+                  <div className="hover:text-[var(--content-secondary)]">Home</div>
+                
+                  <div className="body-s">An Intro to my mind</div>
+                </div>
+              
+                <div className="flex flex-row gap-2">
+                  <div className="hover:text-[var(--content-secondary)]">Projects</div>
+            
+                  <div className="body-s">Lorem Ipsum dolor</div>
+                </div>
+              </div>  
+              
+             </div>
+
+              <div className="flex flex-col gap-2 label-s text-[var(--content-primary)]">
+               <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">
+                  X (Twitter)
+               </a>
+               <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">
+                  GitHub
+               </a>
+              </div>
+            
           </div>
         </div>
       </footer>
