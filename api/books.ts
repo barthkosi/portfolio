b39937415d3a2160d89ad66588e6edd5c1c77a74
Barth <workwithbarth@gmail.com>
2025-12-15 00:39:33 +0100

    chore(reading): update books

--- api/books.ts
+++ api/books.ts
@@ -17,10 +17,14 @@ export default async function handler(
 
   const books = response.results.map((page: any) => ({
     id: page.id,
-    title: page.properties.Title.title[0]?.plain_text ?? "",
-    author: page.properties.Author.rich_text[0]?.plain_text ?? "",
-    image: page.properties.Cover.url ?? "",
+    title: page.properties?.Title?.title?.[0]?.plain_text ?? "Untitled",
+    author: page.properties?.Author?.rich_text?.[0]?.plain_text ?? "Unknown",
+    image:
+      page.properties?.Cover?.files?.[0]?.file?.url ??
+      page.properties?.Cover?.files?.[0]?.external?.url ??
+      "",
   }))
+  
 
   res.status(200).json(books)
 }
