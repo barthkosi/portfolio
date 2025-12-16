ab9b47b42437a6adacb2321631cfafa2679185b0
Barth <workwithbarth@gmail.com>
2025-12-16 16:39:52 +0100

    chore(home): update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -76,7 +76,7 @@ export default function Home() {
                   
                 </div>
       </div>
-      <div className="w-full flex flex-col gap-4">
+      <div className="w-full flex flex-col gap-2 md:gap-3">
         {simple.map(simplecard => (
           <SimpleCard
             key={simplecard.id}
