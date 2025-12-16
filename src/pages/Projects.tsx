6e59a036581779604604e74d78a8ca43c11f1693
Barth <workwithbarth@gmail.com>
2025-12-16 16:32:47 +0100

    style(work): refine projects styling

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,12 +1,12 @@
-import InfoBlock from "../components/InfoBlock";
+
 
 export default function Projects() {
 
   return (
     <main>
-      <div className="flex flex-col justify-center my-auto items-center w-full gap-7 h-auto">
+      <div className="flex flex-col my-auto items-center w-full gap-7">
           
-        <h3>Coming Soon!</h3>
+        <h3 className="my-auto h-full">Coming Soon!</h3>
         
       </div>
     </main>
