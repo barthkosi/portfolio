9d7f6f5dff3bff51de8a77f5eee50c2843b12377
Barth <workwithbarth@gmail.com>
2025-12-17 19:15:33 +0100

    chore(work): update projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,12 +1,10 @@
 export default function Projects() {
 
   return (
-    <main>
-      <div className="flex flex-col my-auto items-center w-full gap-7">
-          
-        <h3 className="my-auto h-full">Coming Soon!</h3>
-        
-      </div>
+    <main className="flex flex-col items-center">
+                
+     <h3 className="flex-1">Coming Soon!</h3>
+            
     </main>
   );
 }
