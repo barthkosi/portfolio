d0e87ae84b1eae8b164f66aff238b9e5b27eb094
Barth <workwithbarth@gmail.com>
2026-01-01 22:03:06 +0100

    chore(writing): update writing

--- src/pages/Writing.tsx
+++ src/pages/Writing.tsx
@@ -9,7 +9,6 @@ export default function Home() {
     <main>
       <div className="flex flex-col my-auto items-center w-full gap-7">
         <h3 className="my-auto h-full">Coming Soon!</h3>
-
       </div>
     </main>
   );
