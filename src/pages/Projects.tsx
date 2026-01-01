a7213c24291d58c382624b20021b8892bf548d1d
Barth <workwithbarth@gmail.com>
2026-01-01 22:38:11 +0100

    chore(work): update projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -17,7 +17,7 @@ export default function Projects() {
               />
       <div className="flex flex-col gap-12">
          <div className="flex flex-col gap-5">
-          <h5 className="w-full max-w-[640px]">An <span className="text-[#31449B]">AI</span> powered <span className="text-[#31449B]">Trip Planner</span> and document organizer
+          <h5 className="w-full max-w-[520px]">An <span className="text-[#31449B]">AI</span> powered <span className="text-[#31449B]">Trip Planner</span> and document organizer
           </h5>
           <div className="flex flex-col md:flex-row gap-1 md:gap-2">
             <div className="w-full flex flex-col">
@@ -30,7 +30,7 @@ export default function Projects() {
           </div>
          </div>
          <div className="flex flex-col gap-5">
-          <h5 className="w-full max-w-[640px]">A fully customizable graphic interface for <span className="text-[#B98D00]">manga</span> and <span className="text-[#7497BB]">comics</span>.
+          <h5 className="w-full max-w-[520px]">A fully customizable graphic interface for <span className="text-[#B98D00]">manga</span> and <span className="text-[#7497BB]">comics</span>.
           </h5>
           <div className="flex flex-col md:flex-row gap-1 md:gap-2">
             <div className="w-full flex flex-col">
@@ -43,7 +43,7 @@ export default function Projects() {
           </div>
          </div>
          <div className="flex flex-col gap-5">
-          <h5 className="w-full max-w-[640px]">Graphics and event banners for the <span className="text-[#0396FF]">sui</span> x <span className="text-[#FE6100]">axelar</span> event in Lagos, NG.
+          <h5 className="w-full max-w-[520px]">Graphics and event banners for the <span className="text-[#0396FF]">sui</span> x <span className="text-[#FE6100]">axelar</span> event in Lagos, NG.
           </h5>
           <div className="flex flex-col md:flex-row gap-1 md:gap-2">
             <div className="w-full flex flex-col">
