bf95591a69328ece6b2958ecefb54da3b77df003
Barth <workwithbarth@gmail.com>
2026-01-06 14:21:46 +0100

    chore(writing): update writing

--- src/pages/Writing.tsx
+++ src/pages/Writing.tsx
@@ -66,7 +66,7 @@ export default function Writing() {
         <InfoBlock
           title="Writing"
           number={posts.length}
-          description="This is a collection of my internal monologues expressed outward, covering everything from tech to my philosophies on life and the messy ideologies of the human condition."
+          description="My internal monologues externalized, covering everything from tech to the messy human condition."
           onComplete={() => setIntroFinished(true)}
         />
 
