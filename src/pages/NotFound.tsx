317083fcfec86a67d63e9933e0438f3515b6b3b7
Barth <workwithbarth@gmail.com>
2026-01-18 23:44:49 +0100

    update: added illustrations and changes styling for 404 page.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,21 +5,20 @@ export default function NotFound() {
 
 
     return (
-        <div className="flex flex-col items-center justify-center h-[calc(100vh-96px)] lg:h-[calc(100vh-166px)] text-center px-4">
+        <div className="flex flex-col lg:flex-row items-center justify-center h-[calc(100vh-96px)] lg:h-[calc(100vh-166px)] px-4 text-[var(--content-primary)]">
             <Head title="barthkosi - 404" description="Page Not Found" />
-            <h1 className="text-[120px] md:text-[180px] lg:text-[240px] font-bold leading-none mb-4 text-[var(--content-primary)]">
-                404
-            </h1>
-            <h2 className="mb-4 text-[var(--content-secondary)]">
+            <img src="https://res.cloudinary.com/barthkosi/image/upload/portfolio-404.webp" alt="404" className="w-fill max-w-[800px] h-auto -ml-[64px] lg:-ml-[96px]" />
+            <div className="flex flex-col items-start">
+            <h2 className="mb-4">
                 Page Not Found
             </h2>
-            <p className="body-l mb-8 max-w-md text-[var(--content-secondary)]">
+            <p className="body-l mb-8 max-w-md">
                 Oops! The page you're looking for doesn't exist. It might have been moved or deleted.
             </p>
-
             <Button to="/">
                 Go to Home
             </Button>
+            </div>
         </div>
     );
 }
