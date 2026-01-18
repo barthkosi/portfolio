3a01832dc662e1810085835af01b24d49e98a00e
Barth <workwithbarth@gmail.com>
2026-01-18 23:52:26 +0100

    refactor: improved styling in 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,10 +5,10 @@ export default function NotFound() {
 
 
     return (
-        <div className="flex flex-col lg:flex-row items-center justify-center h-[calc(100vh-96px)] lg:h-[calc(100vh-166px)] px-4 text-[var(--content-primary)]">
+        <div className="w-fill flex flex-col lg:flex-row gap-8 lg:gap-0 items-center justify-center h-[calc(100vh-96px)] lg:h-[calc(100vh-166px)] px-4 text-[var(--content-primary)]">
             <Head title="barthkosi - 404" description="Page Not Found" />
-            <img src="https://res.cloudinary.com/barthkosi/image/upload/portfolio-404.webp" alt="404" className="w-fill max-w-[800px] h-auto -ml-[64px] lg:-ml-[96px]" />
-            <div className="flex flex-col items-start">
+            <img src="https://res.cloudinary.com/barthkosi/image/upload/portfolio-404.webp" alt="404" className="w-ful lg:w-2/3 h-auto -ml-[64px] lg:-ml-[96px]" />
+            <div className="w-full lg:w-1/3 flex flex-col items-start">
             <h2 className="mb-4">
                 Page Not Found
             </h2>
