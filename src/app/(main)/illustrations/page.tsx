fb678dd6fb4eb0c7938e8fe7c9c33f669cf1fb08
Barth <workwithbarth@gmail.com>
2026-01-30 12:54:24 +0100

    feat: add Open Graph for illustrations page.

--- src/app/(main)/illustrations/page.tsx
+++ src/app/(main)/illustrations/page.tsx
@@ -4,6 +4,13 @@ import IllustrationsContent from "@/components/pages/IllustrationsContent";
 export const metadata: Metadata = {
     title: "barthkosi - illustrations",
     description: "A visual diary of forms. I believe only in continued iteration.",
+    openGraph: {
+        images: [
+            {
+                url: "https://res.cloudinary.com/barthkosi/image/upload/v3-illustrations-og.webp",
+            },
+        ],
+    },
 };
 
 export default function IllustrationsPage() {
