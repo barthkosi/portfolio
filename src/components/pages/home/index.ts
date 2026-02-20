57ae50193331345d151d2587bea9b5581494c4d9
Barth <workwithbarth@gmail.com>
2026-02-20 21:25:53 +0100

    feat: files cleanup.

--- src/components/pages/home/index.ts
+++ src/components/pages/home/index.ts
@@ -3,4 +3,3 @@ export { default as FeaturedWorkSection } from './FeaturedWorkSection';
 export { default as ExplorationsSection } from './ExplorationsSection';
 export { default as WritingSection } from './WritingSection';
 export { default as AboutSection } from './AboutSection';
-export { default as IllustrationsSection } from './IllustrationsSection';
