71aa23209871d36546755641fcd59adb1f08f7bd
Barth <workwithbarth@gmail.com>
2026-06-08 05:57:26 +0100

    fix(transition): adjust z-index to render transition overlay behind navbar

--- src/components/SharedMediaTransitionProvider.tsx
+++ src/components/SharedMediaTransitionProvider.tsx
@@ -210,7 +210,7 @@ export default function SharedMediaTransitionProvider({
                     <AnimatePresence>
                         {transition && (
                             <motion.div
-                                className="fixed inset-0 z-[105] pointer-events-none"
+                                className="fixed inset-0 z-40 pointer-events-none"
                                 initial={{ opacity: 1 }}
                                 animate={{ opacity: 1 }}
                                 exit={{ opacity: 0 }}
